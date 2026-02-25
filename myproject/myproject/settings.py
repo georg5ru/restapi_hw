437c0ab957d1969d75ce1e835c012cda36f4fced
georg5ru <georg555ru@gmail.com>
2026-02-25 17:25:08 +0300

    donee

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = "myproject.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myproject_db',
+        'USER': 'postgres',
+        'PASSWORD': '563669ru',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
